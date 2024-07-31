32983911332b7527a03f9f2b736fc5d126046a75
payetools <143715134+payetools@users.noreply.github.com>
2024-07-31 06:50:14 +0200

    feat: added RTI response type enum

---
+++ src/Payetools.Hmrc.Common/Rti/Model/RtiResponseType.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Hmrc.Common.Rti.Model;
+
+/// <summary>
+/// Enumeration that represents the various types of response that can be received from
+/// the Transaction Engine.
+/// </summary>
+public enum RtiResponseType
+{
+    /// <summary>An unrecognised response was received from the Transaction Engine.</summary>
+    Unrecognised,
+
+    /// <summary>Indicates that the original submission was rejected by the Transaction Engine.</summary>
+    SubmissionRejection,
+
+    /// <summary>Indicates that the original submission was acknowledged by the Transaction Engine. The Transaction
+    /// Engine must be polled for a response once the business system has digested the submission.</summary>
+    SubmissionAcknowledgement,
+
+    /// <summary>Indicates that the original submission has been processed by the Transaction Engine and
+    /// the downstream business system and a business response has been returned.</summary>
+    SubmissionResponse,
+
+    /// <summary>Indicates that a delete request has been actioned by the Transaction Engine.</summary>
+    DeleteResponse,
+
+    /// <summary>
+    /// Indicates that an error occurred and the response includes pertinent error information.
+    /// </summary>
+    ErrorResponse
+}
