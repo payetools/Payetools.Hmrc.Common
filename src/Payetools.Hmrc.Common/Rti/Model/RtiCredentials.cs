f552463cf935c266b788334864331910a73f5d8d
payetools <143715134+payetools@users.noreply.github.com>
2024-08-10 13:20:36 +0200

    fix: added missing default constructor for serialisation

--- src/Payetools.Hmrc.Common/Rti/Model/RtiCredentials.cs
+++ src/Payetools.Hmrc.Common/Rti/Model/RtiCredentials.cs
@@ -1,7 +1,8 @@
-﻿// Copyright (c) 2023-2024 Payetools Foundation.  All rights reserved.
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
 //
-// This source code is the intellectual property of Payetools Foundation
-// and for information security purposes is classified as CONFIDENTIAL.
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
 
 using Payetools.Hmrc.Common.Rti.Model;
 
@@ -16,13 +17,20 @@ public class RtiCredentials : IRtiCredentials
     /// Gets the sender identifier for this set of credentials.  This is typically the
     /// Government Gateway ID being used for RTI submissions.
     /// </summary>
-    public string SenderId { get; init; }
+    public string SenderId { get; init; } = default!;
 
     /// <summary>
     /// Gets the password.  This is typically the Government Gateway password being
     /// used for RTI submissions.
     /// </summary>
-    public string Password { get; init; }
+    public string Password { get; init; } = default!;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RtiCredentials"/> class.
+    /// </summary>
+    public RtiCredentials()
+    {
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="RtiCredentials"/> class.
