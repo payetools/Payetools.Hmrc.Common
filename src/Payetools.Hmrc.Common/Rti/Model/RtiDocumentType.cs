c2e0442faa0906a205e0e3aa1bea1acc69edd483
payetools <143715134+payetools@users.noreply.github.com>
2024-08-16 06:56:31 +0200

    feat: added rti document type

---
+++ src/Payetools.Hmrc.Common/Rti/Model/RtiDocumentType.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Hmrc.Common.Rti.Model;
+
+/// <summary>
+/// RTI document type enumeration.
+/// </summary>
+public enum RtiDocumentType
+{
+    /// <summary>Employer Payment Summary (EPS).</summary>
+    EmployerPaymentSummary = 0,
+
+    /// <summary>Full Payment Submission (FPS).</summary>
+    FullPaymentSubmission = 1,
+
+    /// <summary>NINO Verification Request (NVR).</summary>
+    NinoVerificationRequest = 2
+}
