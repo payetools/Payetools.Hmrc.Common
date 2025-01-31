8aac8769f6f546ece2de119c513f9707469c0a56
payetools <143715134+payetools@users.noreply.github.com>
2025-01-31 11:55:34 +0100

    feat: added employee payrun result summary (wip)

---
+++ src/Payetools.Hmrc.Common/Rti/Model/IEmployeePayRunResultSummary.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2023-2025, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Hmrc.Common.Rti.Model;
+
+/// <summary>
+/// Interface that provides the summary of the employee pay run result, without the
+/// calculation details normally provided by the payrun calculation engine.  This
+/// interface is more suitable for persisting the results of a pay run.
+/// </summary>
+public interface IEmployeePayRunResultSummary
+{
+}
